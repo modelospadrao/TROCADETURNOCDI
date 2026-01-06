da5918240c3377f6c743ecda117117da465528ba
Emanuel <enelceara2023@gmail.com>
2026-01-05 23:08:06 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -4,7 +4,7 @@
 // Para adicionar novos usuários, copie o modelo abaixo:
 // { usuario: 'XXXX', senha: 'XXXX', nome: 'NOME DO OPERADOR' },
 //
-// IMPORTANTE: 
+// IMPORTANTE:
 // - Usuário: 4 dígitos numéricos
 // - Senha: 4 dígitos numéricos
 // - Nome: Nome que será exibido após login
@@ -12,8 +12,15 @@
 
 const USUARIOS_SISTEMA = [
   { usuario: '4321', senha: '4321', nome: 'ADMINISTRADOR' },
+
   { usuario: '9354', senha: '9354', nome: 'Yasmin' },
   { usuario: '0262', senha: '0262', nome: 'Diana' },
+  { usuario: '9009', senha: '9009', nome: 'Tainara' },
+  { usuario: '8998', senha: '8998', nome: 'Ingrid' },
+  { usuario: '4276', senha: '4276', nome: 'Antônio' },
+  { usuario: '0214', senha: '0214', nome: 'Matheus' },
+  { usuario: '5661', senha: '5661', nome: 'Carlos' },
+  { usuario: '8990', senha: '8990', nome: 'Emanuel' }
 ];
 
 // Função para validar login (não modificar)
